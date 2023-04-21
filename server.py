fe3f508aa4aca6fcf71a839cd4c81fcd6fedba72
NikitaMulyar <ni***ol@mail.ru>
2023-04-21 23:06:15 +0300

    1,2я задача. Мда, треш

--- server.py
+++ server.py
@@ -25,6 +25,30 @@ def auth_handler():
     return key, remember_device
 
 
+def get_wall(vk: vk_api.vk_api.VkApiMethod):
+    response = vk.wall.get(count=5, offset=1)
+    if response['items']:
+        cnt = 1
+        for i in response['items']:
+            print(f'===================== {cnt} =====================')
+            cnt += 1
+            print(i['text'] + ';')
+            d = datetime.fromtimestamp(i["date"]).isoformat().split('T')
+            print(f'date: {d[0]}, time: {d[1].split("Z")[0]}')
+
+
+def get_friends(vk: vk_api.vk_api.VkApiMethod):
+    response = vk.friends.get(fields="bdate, city")
+    res = []
+    if response['items']:
+        for i in response['items']:
+            res.append((i['last_name'], i['first_name'], i.get('bdate', 'None')))
+    cnt = 1
+    for i in sorted(res, key=lambda a: a[0]):
+        print(f'{cnt}. {i[0]} {i[1]}. День рождения: {i[2]}')
+        cnt += 1
+
+
 def main():
     login, password = LOGIN, PASSWORD
     vk_session = vk_api.VkApi(
@@ -39,15 +63,7 @@ def main():
         print(error_msg)
         return
     vk = vk_session.get_api()
-    response = vk.wall.get(count=5, offset=1)
-    if response['items']:
-        cnt = 1
-        for i in response['items']:
-            print(f'===================== {cnt} =====================')
-            cnt += 1
-            print(i['text'] + ';')
-            d = datetime.fromtimestamp(i["date"]).isoformat().split('T')
-            print(f'date: {d[0]}, time: {d[1].split("Z")[0]}')
+    get_friends(vk)
 
 
 if __name__ == '__main__':
