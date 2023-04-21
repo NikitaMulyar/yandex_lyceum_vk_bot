08615a036ca3bd2ca55ed01f3dc335cfa1c5b788
NikitaMulyar <ni***ol@mail.ru>
2023-04-21 23:44:59 +0300

    1,2,3я задача. Мда, треш

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import pprint
 import vk_api
 import json
 from datetime import datetime
-
+import requests
 
 LOGIN = '79775446373'
 PASSWORD = 'antirockwho'
@@ -49,6 +49,12 @@ def get_friends(vk: vk_api.vk_api.VkApiMethod):
         cnt += 1
 
 
+def upload_photo(vk: vk_api.vk_api.VkApiMethod):
+    up = vk_api.VkUpload(vk)
+    up.photo(photos=[f'static/img/{i}.jpeg' for i in range(1, 4)], album_id=291455597,
+             group_id=220062557)
+
+
 def main():
     login, password = LOGIN, PASSWORD
     vk_session = vk_api.VkApi(
@@ -63,7 +69,7 @@ def main():
         print(error_msg)
         return
     vk = vk_session.get_api()
-    get_friends(vk)
+    upload_photo(vk)
 
 
 if __name__ == '__main__':
