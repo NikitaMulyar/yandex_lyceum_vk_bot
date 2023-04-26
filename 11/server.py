8b8e562ef1d879f4f535316e00e3e71eb15e6b52
NikitaMulyar <ni***ol@mail.ru>
2023-04-27 00:19:57 +0300

    фулл

--- 11/server.py
+++ 11/server.py
@@ -3,6 +3,7 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import random
 import requests
+import os
 
 
 API_GEO = '40d1649f-0493-4b70-98ba-98533de7710b'
@@ -61,68 +62,69 @@ def main():
     longpoll = VkBotLongPoll(vk_session, group_id='220148670')
 
     vk = vk_session.get_api()
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            id = event.obj.message['from_id']
-            if id not in USERS_DATA:
-                USERS_DATA[id] = {'place': '', 'state': PLACE_REQUEST, 'type': '',
-                                  'coords': None}
-                vk.messages.send(user_id=id,
-                                 message=f"Привет! Напиши адрес/название местности, а я пришлю тебе ее карту!",
-                                 random_id=random.randint(0, 2 ** 64),
-                                 keyboard=VkKeyboard().get_empty_keyboard())
-                continue
-            if USERS_DATA[id]['state'] == PLACE_REQUEST:
-                USERS_DATA[id]['place'] = event.obj.message['text']
-                try:
-                    r = get_coords(USERS_DATA[id]['place'])
-                    if r == -1:
-                        raise Exception
-                except Exception as e:
-                    print(e)
+    while True:
+        for event in longpoll.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                id = event.obj.message['from_id']
+                if id not in USERS_DATA:
+                    USERS_DATA[id] = {'place': '', 'state': PLACE_REQUEST, 'type': '',
+                                      'coords': None}
                     vk.messages.send(user_id=id,
-                                     message=f"Неверный адрес.",
+                                     message=f"Привет! Напиши адрес/название местности, а я пришлю тебе ее карту!",
                                      random_id=random.randint(0, 2 ** 64),
                                      keyboard=VkKeyboard().get_empty_keyboard())
                     continue
-                USERS_DATA[id]['coords'] = r
-                USERS_DATA[id]['state'] = TYPE_REQUEST
-                vk.messages.send(user_id=id,
-                                 message=f"Теперь выбери тип карты!",
-                                 random_id=random.randint(0, 2 ** 64),
-                                 keyboard=KBRD)
-            elif USERS_DATA[id]['state'] == TYPE_REQUEST:
-                if event.obj.message['text'] not in ['Спутник', 'Гибрид', 'Схема']:
+                if USERS_DATA[id]['state'] == PLACE_REQUEST:
+                    USERS_DATA[id]['place'] = event.obj.message['text']
+                    try:
+                        r = get_coords(USERS_DATA[id]['place'])
+                        if r == -1:
+                            raise Exception
+                    except Exception as e:
+                        print(e)
+                        vk.messages.send(user_id=id,
+                                         message=f"Неверный адрес.",
+                                         random_id=random.randint(0, 2 ** 64),
+                                         keyboard=VkKeyboard().get_empty_keyboard())
+                        continue
+                    USERS_DATA[id]['coords'] = r
+                    USERS_DATA[id]['state'] = TYPE_REQUEST
                     vk.messages.send(user_id=id,
-                                     message=f"Выбери тип карты.",
+                                     message=f"Теперь выбери тип карты!",
                                      random_id=random.randint(0, 2 ** 64),
                                      keyboard=KBRD)
-                    continue
-                tmp = {'Спутник': 'sat', 'Схема': 'map', 'Гибрид': 'sat,skl'}
-                USERS_DATA[id]['type'] = event.obj.message['text']
-                USERS_DATA[id]['state'] = PLACE_REQUEST
-                try:
-                    image = get_map(USERS_DATA[id]['coords'], tmp[USERS_DATA[id]['type']])
-                    if image == -1:
-                        raise Exception
-                except Exception:
+                elif USERS_DATA[id]['state'] == TYPE_REQUEST:
+                    if event.obj.message['text'] not in ['Спутник', 'Гибрид', 'Схема']:
+                        vk.messages.send(user_id=id,
+                                         message=f"Выбери тип карты.",
+                                         random_id=random.randint(0, 2 ** 64),
+                                         keyboard=KBRD)
+                        continue
+                    tmp = {'Спутник': 'sat', 'Схема': 'map', 'Гибрид': 'sat,skl'}
+                    USERS_DATA[id]['type'] = event.obj.message['text']
+                    USERS_DATA[id]['state'] = PLACE_REQUEST
+                    try:
+                        image = get_map(USERS_DATA[id]['coords'], tmp[USERS_DATA[id]['type']])
+                        if image == -1:
+                            raise Exception
+                    except Exception:
+                        vk.messages.send(user_id=id,
+                                         message=f"Не получилось построить карту.",
+                                         random_id=random.randint(0, 2 ** 64),
+                                         keyboard=VkKeyboard().get_empty_keyboard())
+                        return
+                    data = vk.photos.getMessagesUploadServer()
+                    with open('photo.png', mode='wb') as f:
+                        f.write(image)
+                    path = os.path.abspath('./photo.png')
+                    res = requests.post(data['upload_url'].replace('\\', ''), files={'photo': (path, open(path, mode='rb'))}).json()
+                    res = vk.photos.saveMessagesPhoto(photo=res['photo'], server=res['server'],
+                                                      hash=res['hash'])[-1]
                     vk.messages.send(user_id=id,
-                                     message=f"Не получилось построить карту.",
+                                     message=f"Это {USERS_DATA[id]['place']}. Что вы еще хотите увидеть?",
                                      random_id=random.randint(0, 2 ** 64),
-                                     keyboard=VkKeyboard().get_empty_keyboard())
-                    return
-                data = vk.photos.getMessagesUploadServer()
-                with open('photo.png', mode='wb') as f:
-                    f.write(image)
-                path = '/Users/nikita/PycharmProjects/async_lessons/yandex_lyceum_vk_bot/11/photo.png'
-                res = requests.post(data['upload_url'].replace('\\', ''), files={'photo': (path, open(path, mode='rb'))}).json()
-                res = vk.photos.saveMessagesPhoto(photo=res['photo'], server=res['server'],
-                                                  hash=res['hash'])[-1]
-                vk.messages.send(user_id=id,
-                                 message=f"Это {USERS_DATA[id]['place']}. Что вы еще хотите увидеть?",
-                                 random_id=random.randint(0, 2 ** 64),
-                                 keyboard=VkKeyboard().get_empty_keyboard(),
-                                 attachment=f'photo{res["owner_id"]}_{res["id"]}')
+                                     keyboard=VkKeyboard().get_empty_keyboard(),
+                                     attachment=f'photo{res["owner_id"]}_{res["id"]}')
 
 if __name__ == '__main__':
     main()
